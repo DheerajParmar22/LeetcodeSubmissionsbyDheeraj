4f99ee11787779e7f404917fb8d1288e060a3aec
Dheeraj Parmar <54523577+DheerajParmar22@users.noreply.github.com>
2022-07-10 02:05:31 +0530

    Time: 58 ms (69.20%), Space: 14.3 MB (99.35%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,17 +1,32 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        i, j = 0, n - 1
-        # for i in range(len(matrix)):
-        #     for j in range(len(matrix[i][j])):
-        while i < m and ~j:
-            # cell == matrix[i][j]
-            if target == matrix[i][j]:
+        # m = len(matrix)
+        # n = len(matrix[0])
+        # i, j = 0, n - 1
+        # # for i in range(len(matrix)):
+        # #     for j in range(len(matrix[i][j])):
+        # while i < m and ~j:
+        #     # cell == matrix[i][j]
+        #     if target == matrix[i][j]:
+        #         return True
+        #     elif target < matrix[i][j]:
+        #         j -= 1
+        #     else:
+        #         i += 1
+        # return False
+    
+    
+    
+    
+        if matrix == [] or matrix is None:
+            return False
+        col = len(matrix[0])-1
+        row = 0
+        while col>=0 and row<len(matrix):
+            if matrix[row][col] == target:
                 return True
-            elif target < matrix[i][j]:
-                j -= 1
+            elif matrix[row][col]>target:
+                col -= 1
             else:
-                i += 1
+                row += 1
         return False
-        
