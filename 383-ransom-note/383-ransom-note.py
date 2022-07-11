74cb25eebdd16d812899ca2d54b1260b4fb0b45a
Dheeraj Parmar <54523577+DheerajParmar22@users.noreply.github.com>
2022-07-11 16:34:40 +0530

    Time: 47 ms (95.92%), Space: 14 MB (94.00%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,12 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         import collections
-        note,mag = Counter(ransomNote), Counter(magazine)
-        if note & mag == note: return True
-        return False
-#         if Counter(ransomNote)&Counter(magazine) == Counter(magazine):
-#             return True
-#         else:
-#             return False
+        # note,mag = Counter(ransomNote), Counter(magazine)
+        # if note & mag == note: return True
+        # return False
+        
+        
+        if Counter(ransomNote) & Counter(magazine) == Counter(ransomNote):
+            return True
+        else:
+            return False
         
         
