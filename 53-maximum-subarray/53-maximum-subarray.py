44f84bf5a21f84857fb089540513b3495d0ad8bd
Dheeraj Parmar <54523577+DheerajParmar22@users.noreply.github.com>
2022-07-16 00:15:38 +0530

    Time: 1050 ms (54.35%), Space: 28.5 MB (15.80%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,6 +1,22 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
+        # Solution1 - time_complexity is 1194ms
+        curr_sum=-inf
+        best_sum=-inf
+        
+        for i in nums:
+            curr_sum = max(i,curr_sum+i)
+            best_sum = max(best_sum, curr_sum)
+            
+        return best_sum
+    
+    
+    
+    
+    
+
+        # Solution2 - time_complexity is 989ms
         max_seen = max_seen_so_far = nums[0]
         
         for i in range(1,len(nums)):
@@ -13,18 +29,3 @@ class Solution:
                 max_seen_so_far = max_seen
                 
         return max_seen_so_far
-                
-        
-#         curr_sum=-inf
-#         best_sum=-inf
-        
-#         for i in nums:
-#             curr_sum = max(i,curr_sum+i)
-#             best_sum = max(best_sum, curr_sum)
-            
-#         return best_sum
-
-
-
-        
-        
