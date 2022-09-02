47f47be2f84d6de2c751a92993621f926ef53971
Dheeraj Parmar <54523577+DheerajParmar22@users.noreply.github.com>
2022-09-03 01:41:00 +0530

    Time: 69 ms (69.57%), Space: 13.9 MB (84.57%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,9 +1,20 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        output = []
         
+        ans = []
         for i in nums1:
             if i in nums2:
-                output.append(i)
+                ans.append(i)
                 nums2.remove(i)
-        return output
+        return ans
+        
+#         output = []
+        
+#         for i in nums1:
+#             if i in nums2:
+#                 output.append(i)
+#                 nums2.remove(i)
+#         return output
+    
+    
+    
