70ae62ae5013a2b8313809717c70839bd269063c
Dheeraj Parmar <54523577+DheerajParmar22@users.noreply.github.com>
2022-09-13 23:22:01 +0530

    Time: 658 ms (20.95%), Space: 14.1 MB (51.52%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -32,6 +32,16 @@ class Solution:
         #     # count = 0
         # return False
         
+        
+        
+#         Since what we concern about is this condition - "after removing exactly one element, the remaining elements will be strictly increasing or not". Once it meets the above condition, we end our for loop and return True. Otherwise, False is returned.
+
+# The pop(index) will remove the item at the given position in the list and return it. It means if I do it on the original list, it will change the list directly. (i.e., nums.pop(i))
+# So I choose to use copy() instead to keep our original list and initialize the copy list every time for following modification and check. (i.e., l = nums.copy())
+
+# For if-else statement, it means after removing exactly one specific position element, if the remaining elements are strictly increasing, we end the for loop and return True. But if not, we use continue to skip the rest of the code in the current iteration of the loop and continue on with the next iteration.
+
+# Back to #1, if each iteration of the for loop is checked and there is no condition that matches what we want, then False is returned.
         for i in range(len(nums)):
             l = nums.copy()
             l.pop(i)
