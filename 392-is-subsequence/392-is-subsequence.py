1c15fa728ead86300bc781e248a8b6a829ee3c6f
Dheeraj Parmar <54523577+DheerajParmar22@users.noreply.github.com>
2022-09-15 21:29:27 +0530

    Time: 66 ms (19.05%), Space: 14 MB (12.66%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        spointer = tpointer = 0
+        
+        while spointer < len(s) and tpointer < len(t):
+            if s[spointer] == t[tpointer]:
+                spointer += 1
+            tpointer += 1
+            
+        return spointer == len(s) 
+            
+        
